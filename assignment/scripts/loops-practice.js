7aa6526f3f805ef6e9af292483305a81e45942ad
Abdirahman Jama <mustalid21@gmail.com>
2023-07-29 10:14:17 -0500

    made final changes.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -12,6 +12,7 @@ for (let i=0; i<4; i++) {
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
+//For this example you have to change the condition of the loop to log anything less than six.
 console.log('count from 0 to 5');
 for (let i = 0; i<6; i++){
   console.log(i);
@@ -19,6 +20,7 @@ for (let i = 0; i<6; i++){
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
+//For this example you're changing the declaration statement that i=3 to initialize your loop at 3.
 console.log('count from 3 to 5');
 for(let i=3; i<6; i++){
   console.log(i);
@@ -26,6 +28,7 @@ for(let i=3; i<6; i++){
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
+//For this example you're changing the incrementation statement and adding by two.
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 for(let i=2; i<11; i+=2){
   console.log(i);
